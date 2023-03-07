3af936234238d653e573eb2a4a99858e13f553c9
keeghire <keeghi@yahoo.com>
2023-03-07 11:23:40 +0700

    OrganizationService.cs

--- SecPortal.Entities/Repositories/IOrganizationRepository.cs
+++ SecPortal.Entities/Repositories/IOrganizationRepository.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using SecPortal.Entities.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SecPortal.Entities.Repositories
 {
     public interface IOrganizationRepository
     {
-
+        IQueryable<Organization> Gets();
     }
 }
---
+++ SecPortal.Services/Services/UserServices/OrganizationService.cs
@@ -0,0 +1,25 @@
+﻿using SecPortal.Entities.Entities;
+using SecPortal.Entities.Infrastructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecPortal.Services.Services.UserServices
+{
+    public class OrganizationService
+    {
+        private IDataContext _dataContext;
+
+        public OrganizationService(IDataContext context) { 
+            _dataContext = context;
+        }
+
+        public List<Organization> GetOrganizations()
+        {
+            var result = _dataContext.Organizations.Gets();
+            return result.ToList();
+        }
+
+    }
+}
